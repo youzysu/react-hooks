b7b39ad07cf30e28827638862c07b63364f6250e
Jisoo Yoo <youzysu@gmail.com>
2023-09-28 13:57:46 +0900

    feat: react 설치

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(<App />);
+
+export function renderApp() {
+  root.render(<App />);
+}
